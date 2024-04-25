d14d182f024b10c642be6975f0a4f6571bc3e5d2
Patrick Adams <patrickadams19@hotmail.com>
2024-04-25 14:48:59 -0400

    initial commit

---
+++ Program.cs
@@ -0,0 +1,81 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace RockPaperScissors
+{
+    class Program
+    {
+
+        public static List<string> Options = new List<string>()
+        {
+            "Rock",
+            "Paper",
+            "Scissors"
+        };
+
+        public static string result(int PlayerChoice, int ComputerChoice)
+        {
+            RPS Player = new RPS(PlayerChoice);
+            RPS Computer = new RPS(ComputerChoice);
+            
+            if(Computer.Weakness == Player.Selection)
+            {
+               return $"Computer chose {Computer.Selection}. You win!";
+            }
+            else if (Computer.Selection == Player.Selection)
+            {
+                return $"Computer chose {Computer.Selection}. Result is a Draw!";
+            }
+            else
+            {
+                return $"Computer chose {Computer.Selection}. You lose!";
+            }
+
+        }
+
+        public static int ComputerMakeChoice()
+        {
+            Random Choice = new Random();
+            return  Choice.Next(1, 4);
+        }
+
+        public static int UserMakeChoice()
+        {
+            int selection;
+
+            Console.WriteLine("\r\nEnter a number 1-3 to make a selection.\r\n");
+
+            for (int i = 1; i < 4; i++)
+            {
+                Console.WriteLine($"\t{i}.{Options[i - 1]}");
+            }
+
+            selection = Convert.ToInt32(Console.ReadLine());
+
+            return selection;
+
+        }
+
+        static void Main(string[] args)
+        {
+            bool play = true;
+            int PlayerChoice;
+            int ComputerChoice;
+            Console.WriteLine("Lets play rock, paper scissors!\r\n");
+
+            while(play)
+            {
+                ComputerChoice = ComputerMakeChoice();
+                PlayerChoice = UserMakeChoice();
+                Console.WriteLine(result(PlayerChoice, ComputerChoice));
+
+                Console.WriteLine("\r\nPlay again? Y/N");
+                if (Console.ReadLine().ToUpper() == "N")
+                {
+                    break;
+                }
+            }
+
+        }
+    }
+}
---
+++ RPS.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RockPaperScissors
+{
+    class RPS
+    {
+        public int ID
+        {
+            get;
+            set;
+        }
+
+        public string Selection
+        {
+            get 
+            {
+                switch (ID)
+                {
+                    case 1:
+                        return "Rock";
+                    case 2:
+                        return "Paper";
+                    case 3:
+                        return "Scissors";
+                    default:
+                        return "Undefined";
+                }
+            }
+        }
+
+        public string Weakness
+        {
+            get 
+            { 
+                switch(Selection)
+                {
+                    case "Rock":
+                        return "Paper";
+                    case "Paper":
+                        return "Scissors";
+                    case "Scissors":
+                        return "Rock";
+                    default:
+                        return "Undefined";
+                }
+            }
+        }
+
+
+       public RPS(int id)
+        {
+            ID = id;
+        }
+    }
+}
